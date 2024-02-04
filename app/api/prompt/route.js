f12bf5f06e09e2ee3006273e052ec93bc16275ed
ZAIN UL ABIDEEN <100013909+zain-2002@users.noreply.github.com>
2024-02-05 02:22:12 +0500

    Update route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,12 +5,16 @@ export const GET = async (req) => {
         await connectToDB();
         const prompts = await Prompt.find({}).populate('creator');
         return new Response(JSON.stringify(prompts), {
-            status: 200
+            status: 200,
+            headers: {
+                'Cache-Control': 'no-store, max-age=0',
+            },
         });
     } catch (error) {
         console.error('Error fetching prompts:', error);
         return new Response('Failed to fetch all prompts', {
-            status: 500
+            status: 500,
         });
     }
 };
+
