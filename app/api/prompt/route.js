093718d4ee64a5417c7cd1a885911b86e1cfd389
ZAIN UL ABIDEEN <100013909+zain-2002@users.noreply.github.com>
2024-02-05 04:24:16 +0500

    correction

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,17 @@
 import Prompt from '@models/prompt';
 import { connectToDB } from '@utils/database';
-
-export const GET = async (req) => {
+import {  NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+export const dynamic = 'force-dynamic';
+export const GET = async (request) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate('creator');
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-    });
+    const path = request.nextUrl.searchParams.get("path") || "/";
+
+    revalidatePath(path);
+
+    return NextResponse.json(prompts);
   } catch (error) {
     console.error('Error fetching prompts:', error);
     return new Response('Failed to fetch all prompts', {
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt",{cache:'no-cache',next:{revalidate:5}});
+    const response = await fetch("/api/prompt",{cache:'no-cache',next:{revalidate:1}});
     const data = await response.json();
 console.log(data);
     setAllPosts(data);
