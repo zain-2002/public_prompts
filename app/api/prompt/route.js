22ee155bb2d02b5cd4a0cbb8de9d18c48c4e5fcd
ZAIN UL ABIDEEN <100013909+zain-2002@users.noreply.github.com>
2024-02-05 03:18:52 +0500

    bug fixed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,20 +1,17 @@
-import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database"
-export const GET = async (req) => {
-    try {
-        await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator');
-        return new Response(JSON.stringify(prompts), {
-            status: 200,
-            headers: {
-                'Cache-Control': 'no-store, max-age=0',
-            },
-        });
-    } catch (error) {
-        console.error('Error fetching prompts:', error);
-        return new Response('Failed to fetch all prompts', {
-            status: 500,
-        });
-    }
-};
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
 
+export const GET = async (req) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate('creator');
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error('Error fetching prompts:', error);
+    return new Response('Failed to fetch all prompts', {
+      status: 500,
+    });
+  }
+};
--- utils/database.js
+++ utils/database.js
@@ -1,19 +1,23 @@
-import mongoose from 'mongoose'
-let isConnected=false;
-export const connectToDB=async()=>{
-    mongoose.set('strictQuery',true)
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI,{
-            dbName:"share_prompt",
-           
-        })
-        isConnected=true;
-        console.log("connected to MongoDB");
-    } catch (error) {
-        console.log(error);
-    }
-}
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_prompt',
+    });
+
+    isConnected = true;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
