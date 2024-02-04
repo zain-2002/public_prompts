b613e1064b444c6422a4cff55d56f2f5f565debd
ZAIN UL ABIDEEN <100013909+zain-2002@users.noreply.github.com>
2024-02-05 02:14:28 +0500

    some updation

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,15 +1,16 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database"
-export const GET=async (req)=>{
+export const GET = async (req) => {
     try {
         await connectToDB();
-        const prompts=await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts),{
-            status:200
-        })
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts), {
+            status: 200
+        });
     } catch (error) {
-        return new Response('Failed to fetch all prompts',{
-            status:500
-        })
+        console.error('Error fetching prompts:', error);
+        return new Response('Failed to fetch all prompts', {
+            status: 500
+        });
     }
-}
+};
